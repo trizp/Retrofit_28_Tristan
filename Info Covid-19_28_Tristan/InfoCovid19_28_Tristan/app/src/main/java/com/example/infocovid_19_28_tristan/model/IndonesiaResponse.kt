5827a0e7bafdac7250d1595153dc673de8c5d080
Tristan Satria <88181731+trizp@users.noreply.github.com>
2022-02-21 14:48:01 +0700

    Add files via upload

---
+++ Info Covid-19_28_Tristan/InfoCovid19_28_Tristan/app/src/main/java/com/example/infocovid_19_28_tristan/model/IndonesiaResponse.kt
@@ -0,0 +1,11 @@
+package com.example.infocovid_19_28_tristan.model
+
+data class IndonesiaResponse(
+    val name: String,
+    val positif: String,
+    val sembuh: String,
+    val meninggal: String,
+    val dirawat: String
+
+
+)
---
+++ Info Covid-19_28_Tristan/InfoCovid19_28_Tristan/app/src/main/java/com/example/infocovid_19_28_tristan/model/ProvinceResponse.kt
@@ -0,0 +1,5 @@
+package com.example.infocovid_19_28_tristan.model
+
+data class ProvinceResponse(
+    val attributes: Province
+)
